13bf66148cfd7ea744b11c09e8eb9ac242c91001
VinayakaOB <vinayaka.ob@lincode.ai>
2023-01-07 14:41:50 +0530

    added routing for misc

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,13 @@ import AuthGuard from './middleware/Guard/AuthGuard';
 import AuthRoute from './pages/auth/AuthRoute';
 import AdminRoute from './pages/admin/AdminRoute';
 import MainRoutes from './pages/misc/MainRoutes';
-import NotFound from './pages/misc/NotFound';
 
 function App() {
 
   return (
     <Router>
       <Routes>
-        <Route path="/*" element={<NotFound/>} />
-        <Route path="/" exact={true} element={<AuthGuard isLoggedIn={true}><MainRoutes /></AuthGuard>} />
+        <Route path="/*" exact={true} element={<AuthGuard isLoggedIn={true}><MainRoutes /></AuthGuard>} />
         <Route path="/auth/*" element={<AuthRoute />} />
         <Route path="/admin/*" element={<AuthGuard isLoggedIn={true}><AdminRoute /></AuthGuard>} />
       </Routes>
--- client/src/pages/misc/MainRoutes.js
+++ client/src/pages/misc/MainRoutes.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 import Header from "../../common/Header"
 import Footer from "../../common/Footer"
+import Articles from './Articles/Articles'
+import Home from './Home/Home'
+import Profile from './Profile/Profile'
+import ProductRoutes from './Product/ProductRoutes'
+import AboutUs from './Others/AboutUs'
+import ContactUs from './Others/ContactUs'
 const MainRoutes = () => {
   return (
     <>
       <Header />
-      <div>MainRoutes</div>
+      <Routes>
+        <Route path="/*" element={<Home />} />
+        <Route path="/articles/*" element={<Articles />} />
+        <Route path="/profile/*" element={<Profile />} />
+        <Route path="/product/*" element={<ProductRoutes />} />
+        <Route path="/about/*" element={<AboutUs />} />
+        <Route path="/contact/*" element={<ContactUs />} />
+      </Routes>
       <Footer />
     </>
   )
