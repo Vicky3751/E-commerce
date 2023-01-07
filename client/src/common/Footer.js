144ad4249bce9112585a9885e7bb37e9d600efe2
VinayakaOB <vinayaka.ob@lincode.ai>
2023-01-07 10:51:10 +0530

    added footer

--- client/src/common/Footer.js
+++ client/src/common/Footer.js
@@ -6,37 +6,6 @@ const Footer = () => {
     return (
         <footer className="footer-section">
             <div className="container">
-                {/* <div className="footer-cta pt-5 pb-5">
-                    <div className="row">
-                        <div className="col-xl-4 col-md-4 mb-30">
-                            <div className="single-cta">
-                                <i className="fas fa-map-marker-alt"></i>
-                                <div className="cta-text">
-                                    <h4>Find us</h4>
-                                    <span>1010 Avenue, sw 54321, chandigarh</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-xl-4 col-md-4 mb-30">
-                            <div className="single-cta">
-                                <i className="fas fa-phone"></i>
-                                <div className="cta-text">
-                                    <h4>Call us</h4>
-                                    <span>9876543210 0</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-xl-4 col-md-4 mb-30">
-                            <div className="single-cta">
-                                <i className="far fa-envelope-open"></i>
-                                <div className="cta-text">
-                                    <h4>Mail us</h4>
-                                    <span>mail@info.com</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div> */}
                 <div className="footer-content pt-5 pb-5 footer-cta">
                     <div className="row">
                         <div className="col-xl-5 col-lg-5 mb-50">
@@ -86,21 +55,6 @@ const Footer = () => {
                                 </ul>
                             </div>
                         </div>
-                        {/* <div className="col-xl-2 col-lg-2 col-md-6 mb-30">
-                            <div className="footer-widget">
-                                <div className="footer-widget-heading">
-                                    <h3>Useful Links</h3>
-                                </div>
-                                <ul>
-                                    <li><a href="#">Home</a></li>
-                                    <li><a href="#">about</a></li>
-                                    <li><a href="#">services</a></li>
-                                    <li><a href="#">portfolio</a></li>
-                                    <li><a href="#">Contact</a></li>
-                                    
-                                </ul>
-                            </div>
-                        </div> */}
                         <div className="col-xl-3 col-lg-3 col-md-6 mb-50">
                             <div className="footer-widget">
                                 <div className="footer-widget-heading">
